4b3387016432308dc52dcae9008a37808dd8fad6
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2019-11-28 20:17:25 +0530

    removed

--- software/Lora_LMIC_vcs/README.md
+++
@@ -1,4 +0,0 @@
-# Instruction
-
-* Use [template.h](src/template.h) to enable and disable functionality.
-
--- software/Lora_LMIC_vcs/src/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMMON_H
-#define _COMMON_H
-void wifi_ota_setup();
-void wifi_ota_loop();
-
-void lmic_setup();
-void lmic_loop();
-
-#endif // !_COMMON_H
